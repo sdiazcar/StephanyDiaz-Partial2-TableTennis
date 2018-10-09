23fd7107fecfce939fa0617e080141a23a0f7262
Stephany Diaz Cartagena <asocoba96@hotmail.com>
2018-10-09 18:59:55 -0500

    1

---
+++ StephanyDiaz-TableTennis/Assets/Scripts/Ball.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    public AudioSource Hitball;
+    void OnCollisionEnter(Collision collision)
+    
+    {
+     
+    }
+    // Use this for initialization
+    void Start () {
+        Hitball = GetComponent<AudioSource>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        {
+        }
+        Debug.Log("Collision detectada");
+        {
+            Hitball.Play();
+        }
+        }
+       
+    }
+
---
+++ StephanyDiaz-TableTennis/Assets/Scripts/Paddle1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle1 : MonoBehaviour
+{
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+            this.transform.Translate(Vector3.up*speed);
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            this.transform.Translate(Vector3.down * speed);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            this.transform.Translate(Vector3.left * speed);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            this.transform.Translate(Vector3.right * speed);{
+
+        }
+
+    }
+
+}
